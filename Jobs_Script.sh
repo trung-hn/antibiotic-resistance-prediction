56050b62d5183f71cabfda47ac9900aa2927e3b5
Trung Hoang <trung.nang.hoang@gmail.com>
2020-02-28 16:07:36 -0500

    Added Jobs_Script.sh

---
+++ Jobs_Script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+### tell SGE to use bash for this script
+#$ -S /bin/bash
+### execute the job from the current working directory, i.e. the directory in which the qsub command is given
+#$ -cwd
+### join both stdout and stderr into the same file
+#$ -j y
+### set email address for sending job status
+#$ -M tnh48@drexel.edu
+### project - basically, your research group name with "Grp" replaced by "Prj"
+#$ -P rosenclassPrj
+### request 15 min of wall clock time "h_rt" = "hard real time" (format is HH:MM:SS, or integer seconds)
+#$ -l h_rt=02:00:00
+### a hard limit 8 GB of memory per slot - if the job grows beyond this, the job is killed
+#$ -l h_vmem=18G
+### want nodes with at least 6 GB of free memory per slot
+#$ -l m_mem_free=16G
+### select the queue all.q
+#$ -q all.q
+
+. /etc/profile.d/modules.sh
+
+### These four modules must ALWAYS be loaded
+module load shared
+module load proteus
+module load sge/univa
+module load gcc
+
+
+### Whatever modules you used, in addition to the 4 above, 
+### when compiling your code (e.g. proteus-openmpi/gcc)
+### must be loaded to run your code.
+### Add them below this line.
+module load python/anaconda3
+
+jupyter nbconvert --execute "test_nbconvert.ipynb"
+jupyter nbconvert --execute --to notebook "test_nbconvert.ipynb"
+
+echo "hello, world"
