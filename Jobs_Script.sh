bb768b971f1bba7ce43467c86dbfce2543970ca8
Trung Hoang <trung.nang.hoang@gmail.com>
2020-02-28 17:01:32 -0500

    WIP: create confusion matrix

--- Jobs_Script.sh
+++ Jobs_Script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+## dos2unix file
 ### tell SGE to use bash for this script
 #$ -S /bin/bash
 ### execute the job from the current working directory, i.e. the directory in which the qsub command is given
@@ -10,10 +10,12 @@
 #$ -M tnh48@drexel.edu
 ### project - basically, your research group name with "Grp" replaced by "Prj"
 #$ -P rosenclassPrj
+
+#$ -pe openmpi_ib 64
 ### request 15 min of wall clock time "h_rt" = "hard real time" (format is HH:MM:SS, or integer seconds)
-#$ -l h_rt=02:00:00
+#$ -l h_rt=20:00:00
 ### a hard limit 8 GB of memory per slot - if the job grows beyond this, the job is killed
-#$ -l h_vmem=18G
+#$ -l h_vmem=64G
 ### want nodes with at least 6 GB of free memory per slot
 #$ -l m_mem_free=16G
 ### select the queue all.q
@@ -36,5 +38,3 @@ module load python/anaconda3
 
 jupyter nbconvert --execute "test_nbconvert.ipynb"
 jupyter nbconvert --execute --to notebook "test_nbconvert.ipynb"
-
-echo "hello, world"
